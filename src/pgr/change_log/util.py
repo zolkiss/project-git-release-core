2c25a20bd1a5646bdc8a558044590b2506a38783
zolkiss <kisszoltan89@gmail.com>
2026-07-06 19:58:53 +0200

    fix: Circular dependency

--- src/pgr/change_log/util.py
+++ src/pgr/change_log/util.py
@@ -1,4 +1,8 @@
+import datetime
+
 from pgr.conv_commit import ConvCommitDetails
+from pgr.conv_commit.resolver import GroupedConvCommits
+from pgr.interfaces import GitRelease
 
 
 def generate_change_chapters(chapter_title: str, changes: list[list[ConvCommitDetails]],
@@ -33,3 +37,27 @@ def _generate_change_records(changes: list[ConvCommitDetails], include_type: boo
         else:
             change_records.append(f"* {change.description}")
     return change_records
+
+
+def generate_release_log(grouped_commits: GroupedConvCommits, previous_version: GitRelease | None,
+                         new_version: GitRelease) -> str:
+    compare_text = f"{new_version.tag_name}"
+    if previous_version is not None:
+        compare_text = f"{previous_version.tag_name}...{new_version.tag_name}"
+
+    new_changes = f"## [{new_version.tag_name}](https://gitea.raspi.zolkiss.home/KiZoCo/testing-repository-with-releases/compare/{compare_text}) ({datetime.datetime.now().strftime("%Y-%m-%d")})\n"
+    if grouped_commits.has_breaking_change():
+        new_changes += "\n".join(generate_change_chapters("Breaking changes",
+                                                          [grouped_commits.braking_changes]))
+    if grouped_commits.has_feature():
+        new_changes += "\n".join(generate_change_chapters("Features",
+                                                          [grouped_commits.get_features()]))
+    if grouped_commits.has_fix():
+        new_changes += "\n".join(generate_change_chapters("Bugfix(es)",
+                                                          [grouped_commits.get_fixes()]))
+    if grouped_commits.has_other_change():
+        new_changes += "\n".join(generate_change_chapters("Other changes",
+                                                          [grouped_commits.get_other_changes(),
+                                                           grouped_commits.invalid_commits], True))
+    new_changes += "---\n"
+    return new_changes
