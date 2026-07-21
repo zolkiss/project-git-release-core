4c7fc12e41e028554da8dc197a631ba70c157718
zolkiss <kisszoltan89@gmail.com>
2026-07-21 22:58:22 +0200

    test: Adding do_release tests

--- test/project_git_release/core/release_engine/update_version_test.py
+++ test/project_git_release/core/release_engine/update_version_test.py
@@ -5,7 +5,7 @@ from unittest.mock import MagicMock
 import pytest
 
 from project_git_release import ReleaseEngine, Connector
-from project_git_release.classes import GitHashAndMsg, CommitDetails, ShortCommitData
+from project_git_release.classes import GitHashAndMsg, CommitDetails, ShortCommitData, GitRelease, NewVersion
 from project_git_release.common.changelog_generator import ChangelogGenerator
 from project_git_release.git import GitCommander
 from project_git_release.test_classes import DummyConnector
@@ -53,24 +53,26 @@ def gen_version_file(mocker) -> MagicMock:
 
 class ReleaseEngineMocks:
     def __init__(self, engine: ReleaseEngine, git_commander: GitCommander, temp_dir: TemporaryDirectory,
-                 changelog_generator: ChangelogGenerator, connector: Connector):
+                 changelog_generator: ChangelogGenerator, connector: Connector, next_version_spy: MagicMock):
         self.engine = engine
         self.git_commander = git_commander
         self.temp_dir = temp_dir
         self.changelog_generator = changelog_generator
         self.connector = connector
+        self.next_version_spy = next_version_spy
 
 
 @pytest.fixture
-def engine_mock(connector, git_commander, temp_dir, release_config, changelog_generator,
+def engine_mock(mocker, connector, git_commander, temp_dir, release_config, changelog_generator,
                 gen_version_file) -> ReleaseEngineMocks:
+    engine = ReleaseEngine(connector=connector, git_commander=git_commander, temp_dir=temp_dir, config=release_config)
     return ReleaseEngineMocks(
-        engine=ReleaseEngine(connector=connector, git_commander=git_commander, temp_dir=temp_dir,
-                             config=release_config),
+        engine=engine,
         git_commander=git_commander,
         temp_dir=temp_dir,
         changelog_generator=changelog_generator,
-        connector=connector
+        connector=connector,
+        next_version_spy=mocker.spy(engine, "_ReleaseEngine__calculate_next_version")
     )
 
 
@@ -109,7 +111,7 @@ def test_no_latest_release_and_has_no_valid_commits(engine_mock, log_info_spy):
     log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
 
 
-def test_first_release_no_commit_after(engine_mock, log_info_spy):
+def test_first_release_no_commit_after(engine_mock, release_config, log_info_spy):
     engine_mock.connector.get_latest_release = MagicMock(return_value=None)
     engine_mock.git_commander.get_file_history.return_value = [
         ShortCommitData(message="feat: New feature", sha="sha_01"),
@@ -128,7 +130,7 @@ def commit_details_se(hash_and_message: GitHashAndMsg) -> CommitDetails:
                          creation_time=datetime.datetime.fromisocalendar(2025, 1, 1), body=None, footers=[])
 
 
-def test_first_release_has_commit_after(mocker, engine_mock, log_info_spy):
+def test_first_release_has_commit_after(mocker, engine_mock, release_config, log_info_spy):
     mocker.patch.object(engine_mock.connector, "get_latest_release", return_value=MagicMock(return_value=None))
     engine_mock.git_commander.get_file_history.return_value = [
         ShortCommitData(message="chore: Release version 0.1.0", sha="sha_02")
@@ -142,6 +144,49 @@ def test_first_release_has_commit_after(mocker, engine_mock, log_info_spy):
     mocker.patch.object(engine_mock.connector, "get_commit_details", side_effect=commit_details_se)
 
     engine_mock.engine.update_version()
+
+    assert NewVersion("1.0.0", release_config.release_version_prefix) == engine_mock.next_version_spy.spy_return
+
+    with pytest.raises(AssertionError):
+        log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
+
+
+def test_second_release_has_commit_after(mocker, engine_mock, release_config, log_info_spy):
+    mocker.patch.object(engine_mock.connector, "get_latest_release",
+                        return_value=GitRelease("0.0.1", "chore: Release version 0.0.1", "sha_00"))
+    engine_mock.git_commander.get_file_history.return_value = [
+        ShortCommitData(message="chore: Release version 0.1.0", sha="sha_02"),
+        ShortCommitData(message="chore: Release version 0.0.1", sha="sha_00")
+    ]
+    engine_mock.git_commander.get_commits_since_latest_release.return_value = [
+        GitHashAndMsg(message="feat: New Feature One", hash="sha_01"),
+        GitHashAndMsg(message="fix: New Fix One", hash="sha_02"),
+        GitHashAndMsg(message="test: Test", hash="sha_03"),
+        GitHashAndMsg(message="chore!: Very Breaking change", hash="sha_04")
+    ]
+    mocker.patch.object(engine_mock.connector, "get_commit_details", side_effect=commit_details_se)
+
+    engine_mock.engine.update_version()
+
+    assert NewVersion("1.0.0", release_config.release_version_prefix) == engine_mock.next_version_spy.spy_return
+
+    with pytest.raises(AssertionError):
+        log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
+
+
+def test_first_release_has_only_bugfix(mocker, engine_mock, release_config, log_info_spy):
+    mocker.patch.object(engine_mock.connector, "get_latest_release", return_value=MagicMock(return_value=None))
+    engine_mock.git_commander.get_file_history.return_value = [
+        ShortCommitData(message="chore: Release version 0.1.0", sha="sha_02")
+    ]
+    engine_mock.git_commander.get_commits_since_latest_release.return_value = [
+        GitHashAndMsg(message="fix: New Fix One", hash="sha_02")
+    ]
+    mocker.patch.object(engine_mock.connector, "get_commit_details", side_effect=commit_details_se)
+
+    engine_mock.engine.update_version()
+
+    assert NewVersion("0.1.1", release_config.release_version_prefix) == engine_mock.next_version_spy.spy_return
 
     with pytest.raises(AssertionError):
         log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
