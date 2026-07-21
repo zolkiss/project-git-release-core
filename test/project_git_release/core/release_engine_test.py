454c903c661d1e7f2697ab12a9ee46f626d65b96
zolkiss <kisszoltan89@gmail.com>
2026-07-21 22:39:23 +0200

    test: Wrapping mocks

--- test/project_git_release/core/release_engine_test.py
+++ test/project_git_release/core/release_engine_test.py
@@ -4,7 +4,7 @@ from unittest.mock import MagicMock
 
 import pytest
 
-from project_git_release import ReleaseEngine
+from project_git_release import ReleaseEngine, Connector
 from project_git_release.classes import GitHashAndMsg, CommitDetails, ShortCommitData
 from project_git_release.common.changelog_generator import ChangelogGenerator
 from project_git_release.git import GitCommander
@@ -52,61 +52,72 @@ def gen_version_file(mocker) -> MagicMock:
 
 
 class ReleaseEngineMocks:
-    engine: ReleaseEngine
-    git_commander: GitCommander
-    temp_dir: TemporaryDirectory
-    changelog_generator: ChangelogGenerator
+    def __init__(self, engine: ReleaseEngine, git_commander: GitCommander, temp_dir: TemporaryDirectory,
+                 changelog_generator: ChangelogGenerator, connector: Connector):
+        self.engine = engine
+        self.git_commander = git_commander
+        self.temp_dir = temp_dir
+        self.changelog_generator = changelog_generator
+        self.connector = connector
 
 
 @pytest.fixture
-def release_engine(connector, git_commander, temp_dir, release_config, changelog_generator, gen_version_file):
-    return ReleaseEngine(connector=connector, git_commander=git_commander, temp_dir=temp_dir, config=release_config)
+def engine_mock(connector, git_commander, temp_dir, release_config, changelog_generator,
+                gen_version_file) -> ReleaseEngineMocks:
+    return ReleaseEngineMocks(
+        engine=ReleaseEngine(connector=connector, git_commander=git_commander, temp_dir=temp_dir,
+                             config=release_config),
+        git_commander=git_commander,
+        temp_dir=temp_dir,
+        changelog_generator=changelog_generator,
+        connector=connector
+    )
 
 
-def test_no_latest_release_and_no_new_commit(release_engine, connector, log_info_spy):
-    connector.get_latest_release = MagicMock(return_value=None)
+def test_no_latest_release_and_no_new_commit(engine_mock, log_info_spy):
+    engine_mock.connector.get_latest_release = MagicMock(return_value=None)
 
     with pytest.raises(SystemExit) as se:
-        release_engine.update_version()
+        engine_mock.engine.update_version()
         assert 0 == se.value.code
 
     log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
 
 
-def test_no_latest_release_and_no_new_commit_error(release_engine, connector, git_commander, log_error_spy):
-    connector.get_latest_release = MagicMock(return_value=None)
-    git_commander.get_commits_since_latest_release.return_value = None
+def test_no_latest_release_and_no_new_commit_error(engine_mock, log_error_spy):
+    engine_mock.connector.get_latest_release = MagicMock(return_value=None)
+    engine_mock.git_commander.get_commits_since_latest_release.return_value = None
 
     with pytest.raises(SystemExit) as se:
-        release_engine.update_version()
+        engine_mock.engine.update_version()
         assert 1 == se.value.code
 
     log_error_spy.assert_any_call("Error while getting commits since last release...")
 
 
-def test_no_latest_release_and_has_no_valid_commits(release_engine, git_commander, connector, log_info_spy):
-    connector.get_latest_release = MagicMock(return_value=None)
-    git_commander.get_file_history.return_value = [
+def test_no_latest_release_and_has_no_valid_commits(engine_mock, log_info_spy):
+    engine_mock.connector.get_latest_release = MagicMock(return_value=None)
+    engine_mock.git_commander.get_file_history.return_value = [
         ShortCommitData(message="New Commit 01", sha="sha_01"),
         ShortCommitData(message="New Commit 02", sha="sha_02")
     ]
 
     with pytest.raises(SystemExit) as se:
-        release_engine.update_version()
+        engine_mock.engine.update_version()
         assert 0 == se.value.code
 
     log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
 
 
-def test_first_release_no_commit_after(release_engine, git_commander, connector, log_info_spy):
-    connector.get_latest_release = MagicMock(return_value=None)
-    git_commander.get_file_history.return_value = [
+def test_first_release_no_commit_after(engine_mock, log_info_spy):
+    engine_mock.connector.get_latest_release = MagicMock(return_value=None)
+    engine_mock.git_commander.get_file_history.return_value = [
         ShortCommitData(message="feat: New feature", sha="sha_01"),
         ShortCommitData(message="chore: Release version 0.1.0", sha="sha_02")
     ]
 
     with pytest.raises(SystemExit) as se:
-        release_engine.update_version()
+        engine_mock.engine.update_version()
         assert 0 == se.value.code
 
     log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
@@ -117,20 +128,20 @@ def commit_details_se(hash_and_message: GitHashAndMsg) -> CommitDetails:
                          creation_time=datetime.datetime.fromisocalendar(2025, 1, 1), body=None, footers=[])
 
 
-def test_first_release_has_commit_after(mocker, release_engine, git_commander, connector, log_info_spy):
-    mocker.patch.object(connector, "get_latest_release", return_value=MagicMock(return_value=None))
-    git_commander.get_file_history.return_value = [
+def test_first_release_has_commit_after(mocker, engine_mock, log_info_spy):
+    mocker.patch.object(engine_mock.connector, "get_latest_release", return_value=MagicMock(return_value=None))
+    engine_mock.git_commander.get_file_history.return_value = [
         ShortCommitData(message="chore: Release version 0.1.0", sha="sha_02")
     ]
-    git_commander.get_commits_since_latest_release.return_value = [
+    engine_mock.git_commander.get_commits_since_latest_release.return_value = [
         GitHashAndMsg(message="feat: New Feature One", hash="sha_01"),
         GitHashAndMsg(message="fix: New Fix One", hash="sha_02"),
         GitHashAndMsg(message="test: Test", hash="sha_03"),
         GitHashAndMsg(message="chore!: Very Breaking change", hash="sha_04")
     ]
-    mocker.patch.object(connector, "get_commit_details", side_effect=commit_details_se)
+    mocker.patch.object(engine_mock.connector, "get_commit_details", side_effect=commit_details_se)
 
-    release_engine.update_version()
+    engine_mock.engine.update_version()
 
     with pytest.raises(AssertionError):
         log_info_spy.assert_any_call("There is no commit since the latest release. Quitting...")
